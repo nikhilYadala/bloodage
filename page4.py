e22bbf762d6fd753fd55dfcca0da3f9cf8017274
ZhouyaoXie <zx2226@columbia.edu>
2021-12-10 16:14:47 -0500

    added form and metrics to page4

--- page4.py
+++ page4.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 
 title = "Predicting Biological Age"
 
@@ -10,22 +11,38 @@ intro_text = """
 **Train your own age prediction model using gradient boosting decision tree (GBDT) and evalute its performance!**
 """
 
+def train_model():
+	return 0
+
 def app():
 	st.title(title)
 	st.markdown(intro_text)
-	col2, col1 = st.columns(2)
-	col1.slider("Number of Trees:", min_value = 10, max_value = 2000, value = 1000, 
-		help = "The number of decision trees used in the model. \
-			A larger value typically decreases training error but will take longer to train and might cause overfittng.")
-	col1.slider("Depth of Trees:", min_value = 2, max_value = 10, value = 6, 
-		help = "The maximum depth of individual decision trees.\
-		 The maximum depth limits the number of nodes in the tree.")
-	col1.select_slider("Learning rate:", options = [0.001, 0.01, 0.02, 0.05, 0.1], value = 0.01, 
-		help = "Learning rate. This shrinks the contribution of each tree by the specified amount.")
-	col1.slider("Min Splits:", min_value = 10, max_value = 50, value = 30, 
-		help = "The minimum number of samples required to split an internal node of a tree.")
-	
-	col2.markdown("Select sets of features to include in training:")
-	col2.checkbox("Demographics features:", value = True)
-	col2.checkbox("Diseases features:", value = True)
-	col2.checkbox("Biochemistry features:", value = True)
+
+	with st.form(key = 'hyperparameters'):
+		col2, col1 = st.columns(2)
+		col1.slider("Number of Trees:", min_value = 10, max_value = 2000, value = 1000, 
+			help = "The number of decision trees used in the model. \
+				A larger value typically decreases training error but will take longer to train and might cause overfittng.")
+		col1.slider("Depth of Trees:", min_value = 2, max_value = 10, value = 6, 
+			help = "The maximum depth of individual decision trees.\
+			 The maximum depth limits the number of nodes in the tree.")
+		col1.select_slider("Learning rate:", options = [0.001, 0.01, 0.02, 0.05, 0.1], value = 0.01, 
+			help = "Learning rate. This shrinks the contribution of each tree by the specified amount.")
+		col1.slider("Min Splits:", min_value = 10, max_value = 50, value = 30, 
+			help = "The minimum number of samples required to split an internal node of a tree.")
+		
+		col2.markdown("Select sets of features to include in training:")
+		col2.checkbox("Demographics features:", value = True)
+		col2.checkbox("Diseases features:", value = True)
+		col2.checkbox("Biochemistry features:", value = True)
+
+		st.form_submit_button(label = "Train Model!")
+
+	model_training_state = st.markdown("Model training... This might take a few minutes...")
+	start = time.time()
+	re = train_model()
+	end = time.time()
+	model_training_state.markdown(' ')
+	col1, col2, col3, col4 = st.columns(4)
+	col1.metric(label = 'Accuracy', value = 0.974)
+	col2.metric(label = 'Training Time', value = str(int(end-start)) + 's')
